239fd8fad8a2cbc66fad0be0ed6bb0a8594a199b
Darren Brien <darrenbrien@aol.com>
2016-12-24 22:06:37 +0000

    prob 96 solved

---
+++ projeuler/complete/prob96.py
@@ -0,0 +1,57 @@
+
+def read():
+    with open('p096_sudoku.txt') as f:
+        lines = f.readlines()
+        puzzles = []
+        puzzle = []
+        for i, l in enumerate(lines):
+            if i % 10 == 0:
+                if len(puzzle) > 0:
+                    puzzles.append(puzzle)
+                    puzzle = []
+            else:
+                puzzle.append(list(map(int, str.strip(l))))
+        puzzles.append(puzzle)
+        return puzzles
+
+def solve(g):
+    if(solved(g)):
+        return g
+    if(not valid(g)):
+        return None
+    x, *xs = emps(g)
+    i, j = x
+    for c in pos_vals(g, i, j):
+        ng = list(map(list, g))
+        ng[i][j] = c
+        res = solve(ng)
+        if(res):
+            return res
+    return None
+
+def pos_vals(g, i , j):
+    def grid(g, i, j):
+        idx = [0,3,6]
+        start_i = idx[i//3]
+        end_i = start_i + 3
+        start_j = idx[j//3]
+        end_j = start_j + 3
+        l = [g[i][start_j:end_j] for i in range(start_i, end_i)]
+        return [y for x in l for y in x]
+    def col(g, j):
+        return [row[j] for row in g]
+    vals = set(list(range(1,10)))
+    vals = vals - set(g[i])
+    vals = set(vals) - set(col(g,j))
+    return set(vals) - set(grid(g, i, j))
+
+def emps(g):
+    return [(i,j) for i in range(0,9) for j in range(0,9) if g[i][j] == 0]
+
+def valid(g):
+    return all(map(lambda x: pos_vals(g, *x), emps(g)))
+
+def solved(g):
+    return len(emps(g)) == 0
+
+print(sum(map(lambda x: int(''.join(map(str,x[0][0:3]))),[solve(r) for r in read()])))
